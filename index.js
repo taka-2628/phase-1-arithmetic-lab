a29b86525357072774987bafe41acf496c153f93
Takashi <th2826@columbia.edu>
2021-08-10 21:36:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()*10);
+
+/* or
+const random = Math.floor(Math.random()*10)+1;
+*/
+
+let num3 = 18;
+let num4 = 7;
+const mod = num3 % num4;
+
+let max = Math.max(0, 20);
